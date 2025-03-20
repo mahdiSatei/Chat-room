ab2bc54d2239e737829b5f4a33df69143cd823f8
mahdi <mahdisatei82@gmail.com>
2025-03-20 22:17:10 +0330

    add registration to server

---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+HOST = "127.0.0.1"
+PORT = 15000
+ADRR = (HOST, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADRR)
+server.listen(5)
+
+users = {}
+
+def handle_client(client_socket):
+    while True:
+        try :
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+
+            if message.startswith("Registration"):
+                _, username, password = message.split(' ')
+                if username in users:
+                    client_socket.send("User already exists.".encode())
+                else:
+                    users[username] = password
+                    client_socket.send("Succseefuly registred!".encode())
+        except:
+            pass
+        
+while True:
+    client_socket, addr = server.accept()
+    print(f"Connection from {addr} established.")
+    # clients.append(client_socket)
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
