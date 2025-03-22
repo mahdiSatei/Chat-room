917fe68378583bbf9756b36e582619f8f35873eb
mahdi <mahdisatei82@gmail.com>
2025-03-22 13:18:43 +0330

    add bye and change some var names

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ server.bind(ADRR)
 server.listen(5)
 
 users = {}
-onlinUsers = {}
+online_users = {}
 
 def handle_client(client_socket):
     while True:
@@ -31,14 +31,14 @@ def handle_client(client_socket):
                 _, username = message.split(' ')
                 if username in users:
                     client_socket.send("Key 1".encode())
-                    onlinUsers[username] = (client_socket, "1")
+                    online_users[username] = (client_socket, "1")
                 else:
                     client_socket.send("User not found.".encode())
 
             if message.startswith("H"):
                 _, username = message.split(' ')
                 logged_in_user = is_logged_in(client_socket)
-                if username in onlinUsers and logged_in_user:
+                if username in online_users and logged_in_user:
                     broadcast(f"{username} joined the chat room.")
                     client_socket.send(f"\nHi {username}, welcome to the chat room.".encode())
                 else:
@@ -48,7 +48,7 @@ def handle_client(client_socket):
                 username = is_logged_in(client_socket)
                 if username:
                     client_socket.send("Here is the list of attendees:\n\r".encode())
-                    client_socket.send(",".join(onlinUsers.keys()).encode())
+                    client_socket.send(",".join(online_users.keys()).encode())
                 else:
                     client_socket.send("Please login first.".encode())
 
@@ -68,20 +68,36 @@ def handle_client(client_socket):
                     for reciver in recivers:
                         send_private_massage(client_socket, reciver, message_body, recivers)
 
+            if message.startswith("Bye"):
+                username = is_logged_in(client_socket)
+                if username:
+                    broadcast(f"{username} left the chatroom.")
+                    del online_users[username]
+                    client_socket.send("You have left the chatroom.".encode())
+                    client_socket.close()
+                    break
+                else:
+                    client_socket.send("Please login first".encode())
+
         except Exception as e:
-            print(e)
+            username = is_logged_in(client_socket)
+            if username:
+                print(f"User {username} disconnected unexpectedly: {e}")
+                del online_users[username]
+            client_socket.close()
+            break
 
 def broadcast(message):
-    for user, (socket, _) in onlinUsers.items():
+    for user, (socket, _) in online_users.items():
         try:
             socket.send(message.encode())
         except:
             print(f"User {user} disconnected.")
-            del onlinUsers[user]
+            del online_users[user]
             socket.close()
 
 def is_logged_in(client_socket):
-    for username, (socket, _) in onlinUsers.items():
+    for username, (socket, _) in online_users.items():
         if socket == client_socket:
             return username
     return None
@@ -90,8 +106,8 @@ def send_private_massage(sender, reciver, message, recivers):
     sender_username = is_logged_in(sender)
     if reciver not in users:
         sender.send(f"User {reciver} dose not exist.".encode())
-    elif reciver in onlinUsers:
-        reciver_socket = onlinUsers[reciver][0]
+    elif reciver in online_users:
+        reciver_socket = online_users[reciver][0]
         try:
             usernames = ",".join(recivers)
             reciver_socket.send(f"Private message from {sender_username} to {usernames}\r\n".encode())
@@ -101,9 +117,13 @@ def send_private_massage(sender, reciver, message, recivers):
     else:  
         sender.send(f"Error: User {reciver} is not online.".encode())    
 
-while True:
-    client_socket, addr = server.accept()
-    print(f"Connection from {addr} established.")
-    # clients.append(client_socket)
-    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-    client_thread.start()
+def main():
+    print("Server is running...")
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Connection from {addr} established.")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+if __name__ == "__main__":
+    main()
